4856bd5d92d896cf94ee09dd139e093f80691035
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-03 15:03:32 +0100

    add id Cognome and id ColorePreferito to the page

--- js/script.js
+++ js/script.js
@@ -5,14 +5,22 @@ console.log('JS OK!');
 const Nome = prompt('qual\'è il tuo nome?');
 console.log('il tuo nome è ' + Nome);
 
+// accedo all'elemento con id Nome
+document.getElementById('Nome').innerText = 'il tuo nome è: ' + Nome;
+
 //seconda richiesta cognome
 const Cognome = prompt('qual\'è il tuo cognome?')
 console.log('il tuo cognome è ' + Cognome);
+
+// accedo all'elemento con id Cognome
+document.getElementById('Cognome').innerText = 'il tuo cognome è: ' + Cognome;
 
 //terza richiesta colore preferito
 const ColorePreferito = prompt('qual\'è il tuo colore preferito?')
 console.log('il tuo colore preferito è ' + ColorePreferito);
 
-// accedo all'elemento con id Nome
-document.getElementById('Nome').innerText = 'il tuo nome è: ' + Nome;
+// accedo all'elemento con id ColorePreferito
+document.getElementById('ColorePreferito').innerText = 'il tuo cognome è: ' + ColorePreferito;
+
+
 
