bcb3c37eec410cb031d7855d7051af6f97ba7e16
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-03 14:55:08 +0100

    add corrections add id Nome to the page

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@
 console.log('JS OK!');
 
 //prima richiesta nome
-const username = prompt('qual\'è il tuo nome?');
-console.log(username);
+const Nome = prompt('qual\'è il tuo nome?');
+console.log('il tuo nome è ' + Nome);
 
 //seconda richiesta cognome
-const surname = prompt('qual\'è il tuo cognome?')
-console.log(surname);
+const Cognome = prompt('qual\'è il tuo cognome?')
+console.log('il tuo cognome è ' + Cognome);
 
 //terza richiesta colore preferito
-const color = prompt('qual\'è il tuo colore preferito?')
-console.log(color);
+const ColorePreferito = prompt('qual\'è il tuo colore preferito?')
+console.log('il tuo colore preferito è ' + ColorePreferito);
 
+// accedo all'elemento con id Nome
+document.getElementById('Nome').innerText = 'il tuo nome è: ' + Nome;
 
