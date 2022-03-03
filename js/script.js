3f3d4f6f40187537e69c44387e088bdf642beda2
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-03 14:21:05 +0100

    add second request surname

--- js/script.js
+++ js/script.js
@@ -2,5 +2,9 @@
 console.log('JS OK!');
 
 //prima richiesta nome
-const name = prompt('come ti chiami');
-console.log(name)
+const name = prompt('qual\'è il tuo nome?');
+console.log(name);
+
+//seconda richiesta cognome
+const surname = prompt('qual\'è il tuo cognome?')
+console.log(surname);
