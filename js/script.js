d5109c165ceb9dcfddfe2d69d27c05ceab7a077a
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-03 14:32:12 +0100

    add third request favorite color

--- js/script.js
+++ js/script.js
@@ -2,9 +2,15 @@
 console.log('JS OK!');
 
 //prima richiesta nome
-const name = prompt('qual\'è il tuo nome?');
-console.log(name);
+const username = prompt('qual\'è il tuo nome?');
+console.log(username);
 
 //seconda richiesta cognome
 const surname = prompt('qual\'è il tuo cognome?')
 console.log(surname);
+
+//terza richiesta colore preferito
+const color = prompt('qual\'è il tuo colore preferito?')
+console.log(color);
+
+
